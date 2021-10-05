8f680f42aeccea58d01e080b7d0e68d847f4e560
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-05 23:36:27 +0530

    Emp Attendence

--- empcheck/empcheck/Program.cs
+++ empcheck/empcheck/Program.cs
@@ -6,24 +6,18 @@ namespace empcheck
     {
         static void Main(string[] args)
         {
-            int emp_rate_per_hr = 20;
             int is_present = 1;
-            int emp_hr = 0;
-            int emp_wage = 0;
-
             Random random = new Random();
 
             int empcheck = random.Next(0, 2);
-            if(empcheck == is_present)
+            if (empcheck == is_present)
             {
-                emp_hr = 8;
+                Console.WriteLine("employ is present");
             }
             else
             {
-                emp_hr = 0;
+                Console.WriteLine("employ is absent");
             }
-            emp_wage = emp_hr * emp_rate_per_hr;
-            Console.WriteLine("emp_wage :" + emp_wage);
         }
     }
 }
