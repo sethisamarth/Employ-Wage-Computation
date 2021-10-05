2917562da10f1682d01a681f8bea6cae1c0ba81b
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-05 23:57:20 +0530

    add part time EMP

--- empcheck/empcheck/Program.cs
+++ empcheck/empcheck/Program.cs
@@ -6,18 +6,29 @@ namespace empcheck
     {
         static void Main(string[] args)
         {
+            int emp_rate_per_hr = 20;
             int is_present = 1;
+            int is_part_present = 2;
+            int emp_hr = 0;
+            int emp_wage = 0;
+
             Random random = new Random();
 
-            int empcheck = random.Next(0, 2);
+            int empcheck = random.Next(0, 3);
             if (empcheck == is_present)
             {
-                Console.WriteLine("employ is present");
+                emp_hr = 8;
+            }
+            else if (empcheck == is_part_present)
+            {
+                emp_hr = 4;
             }
             else
             {
-                Console.WriteLine("employ is absent");
+                emp_hr = 0;
             }
+            emp_wage = emp_hr * emp_rate_per_hr;
+            Console.WriteLine("emp_wage :" + emp_wage);
         }
     }
 }
