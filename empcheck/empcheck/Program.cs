6f407f8c2358dba442d1ae85ee3a2fe5cc0bbbf7
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-06 11:33:47 +0530

    Switch case

--- empcheck/empcheck/Program.cs
+++ empcheck/empcheck/Program.cs
@@ -7,28 +7,31 @@ namespace empcheck
         static void Main(string[] args)
         {
             int emp_rate_per_hr = 20;
-            int is_present = 1;
-            int is_part_present = 2;
+          int is_present = 1;
+          int is_part_present = 2;
             int emp_hr = 0;
             int emp_wage = 0;
-
             Random random = new Random();
 
-            int empcheck = random.Next(0, 3);
-            if (empcheck == is_present)
+            int empcheck = random.Next(0 , 3);
+            switch (empcheck)
             {
-                emp_hr = 8;
-            }
-            else if (empcheck == is_part_present)
-            {
+                case 1:
+                   emp_hr = 8; 
+                    break;
+                case 2:
                 emp_hr = 4;
+
+                    break;
+                default:
+                    emp_hr = 0;
+
+                    break;
             }
-            else
-            {
-                emp_hr = 0;
-            }
+
             emp_wage = emp_hr * emp_rate_per_hr;
             Console.WriteLine("emp_wage :" + emp_wage);
+            Console.ReadLine();
         }
     }
 }
